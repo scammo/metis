d3bb0da2cec68f268a4d4f82293bd9143e8e85bb
Momme Jürgensen <momme@juergensen.me>
2019-06-12 12:22:00 +0200

    add authentication to feathers

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,5 +1,7 @@
 const box = require('./box/box.service.js');
+const user = require('./user/user.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(box);
+  app.configure(user);
 };
