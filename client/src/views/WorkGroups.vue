4b7bf38624c6c5c13f8cbb351e60993e784d546c
Jonas Riecken <jonas.riecken00@gmail.com>
2019-06-13 14:55:32 +0200

    Update WorkGroups.vue

--- client/src/views/WorkGroups.vue
+++ client/src/views/WorkGroups.vue
@@ -1,5 +1,71 @@
 <template>
   <div>
-    <h1>This is the WorkGroups page.</h1>
+    <div class="container py-4">
+      <div class="row">
+        <router-link
+          :to="{ name: 'warehouselocation'}"
+          class="border col-6 py-2 text-center"
+        >
+          <div>
+            Lagerstandorte verwalten
+          </div>
+        </router-link>
+        <router-link
+          :to="{ name: 'workgroups'}"
+          class="border col-6 py-2 text-center router-link-active"
+        >
+          <div>
+            Arbeitskreise verwalten
+          </div>
+        </router-link>
+      </div>
+      <div class="row px-2 pt-4  border">
+        <div class="col-12">
+          <table class="table">
+            <thead>
+              <tr>
+                <th
+                  scope="col"
+                  width="25%"
+                >
+                  Abkürzung
+                </th>
+                <th
+                  scope="col"
+                >
+                  vollständiger Name
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  Abkürzung
+                </td>
+                <td>
+                  vollständiger Name
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Abkürzung
+                </td>
+                <td>
+                  vollständiger Name
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Abkürzung
+                </td>
+                <td>
+                  vollständiger Name
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
