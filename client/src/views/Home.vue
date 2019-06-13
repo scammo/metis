fb1755e6cc6a64b6ae1c685249f99fc3dd2d7e8c
Jonas Riecken <jonas.riecken00@gmail.com>
2019-06-13 10:11:07 +0200

    removed extra duplication

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -10,31 +10,16 @@
         </router-link>
       </div>
       <div class="border col-6">
-        <h2>
-          KLAPPBARE BIG BOXEN GESCHLOSSEN
-        </h2>
-        <div>
+        <div class="float-left">
+          <h2>
+            KLAPPBARE BIG BOXEN GESCHLOSSEN
+          </h2>
           Eckernförde<br>
           Inhalt: Wurst, Besteck, Glass<br>
           Arbeitskreis: OekA
         </div>
-      </div>
-    </div>
-    <div class="row justify-content-md-center">
-      <div class="border col-1">
-        <img
-          src="https://guardsman.com.au/wp-content/uploads/2018/07/book_wine_plain.png"
-          class="img-fluid"
-        >
-      </div>
-      <div class="border col-6">
-        <h2>
-          KLAPPBARE BIG BOXEN GESCHLOSSEN
-        </h2>
-        <div>
-          Eckernförde<br>
-          Inhalt: Wurst, Besteck, Glass<br>
-          Arbeitskreis: OekA
+        <div class="float-right pt-1 font-weight-bold">
+          26
         </div>
       </div>
     </div>
