6efe2d112fe622a8fe42deaa35dbe365fb2d34cd
Jonas Riecken <jonas.riecken00@gmail.com>
2019-06-13 14:55:28 +0200

    Update WarehouseLocation.vue

--- client/src/views/WarehouseLocation.vue
+++ client/src/views/WarehouseLocation.vue
@@ -1,5 +1,60 @@
 <template>
   <div>
-    <h1>This is the warehouse location page.</h1>
+    <div class="container py-4">
+      <div class="row">
+        <router-link
+          :to="{ name: 'warehouselocation'}"
+          class="border col-6 py-2 text-center router-link-active"
+        >
+          <div>
+            Lagerstandorte verwalten
+          </div>
+        </router-link>
+        <router-link
+          :to="{ name: 'workgroups'}"
+          class="border col-6 py-2 text-center"
+        >
+          <div>
+            Arbeitskreise verwalten
+          </div>
+        </router-link>
+      </div>
+      <div class="row px-2 pt-4  border">
+        <div class="col-12">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">
+                  Ort
+                </th>
+                <th scope="col">
+                  Adresse
+                </th>
+                <th scope="col">
+                  Einschrenkungen
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Ort</td>
+                <td>Adresse</td>
+                <td>Einschrenkungen</td>
+              </tr>
+              <tr>
+                <td>Eckernförde</td>
+                <td>24340 Eckernförde, Musterstraße</td>
+                <td>Keine Paletten</td>
+              </tr>
+              <tr>
+                <td>Eckernförde</td>
+                <td>24340 Eckernförde, Musterstraße 4</td>
+                <td>Keine Paletten</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
