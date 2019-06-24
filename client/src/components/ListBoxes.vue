d183afa6552163cca2b46228add09dd8767b494f
Jonas Riecken <jonas.riecken00@gmail.com>
2019-06-24 11:15:36 +0200

    Exported the List of boxes into a component

---
+++ client/src/components/ListBoxes.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="row pb-4 pt-2">
+    <router-link
+      :to="{ name: 'details'}"
+      class="col-2 float-left border"
+    >
+      <img
+        src="../assets/box.png"
+        class="img-fluid"
+      >
+    </router-link>
+    <div class="col-10 border py-2 pl-4 float-left">
+      <div class="row">
+        <div class="col-10">
+          <h2 class="text-uppercase">
+            <router-link
+              :to="{ name: 'details'}"
+              class="linknobackground"
+            >
+              Klappbare Big Box geschlossen
+            </router-link>
+          </h2>
+          <div class="grey">
+            Eckernförde
+          </div>
+          <div class="row grey">
+            <div class="col-3 font-weight-bold">
+              Titel <br>
+              Kisten Nummer <br>
+              Gewicht <br>
+              Typ
+            </div>
+            <div class="col-4">
+              Titel <br>
+              26 <br>
+              schwer <br>
+              Klappbare Big Box geschlossen
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="float-right font-weight-bold">
+            26
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -9,64 +9,21 @@
           />
         </div>
       </div>
-      <div class="row pb-4 pt-2">
-        <router-link
-          :to="{ name: 'details'}"
-          class="col-2 float-left border"
-        >
-          <img
-            src="../assets/box.png"
-            class="img-fluid"
-          >
-        </router-link>
-        <div class="col-10 border py-2 pl-4 float-left">
-          <div class="row">
-            <div class="col-10">
-              <h2 class="text-uppercase">
-                <router-link
-                  :to="{ name: 'details'}"
-                  class="linknobackground"
-                >
-                  Klappbare Big Box geschlossen
-                </router-link>
-              </h2>
-              <div class="grey">
-                Eckernförde
-              </div>
-              <div class="row grey">
-                <div class="col-3 font-weight-bold">
-                  Titel <br>
-                  Kisten Nummer <br>
-                  Gewicht <br>
-                  Typ
-                </div>
-                <div class="col-4">
-                  Titel <br>
-                  26 <br>
-                  schwer <br>
-                  Klappbare Big Box geschlossen
-                </div>
-              </div>
-            </div>
-            <div class="col-2">
-              <div class="float-right font-weight-bold">
-                26
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ListBoxes />
+      <create-box-modal />
+      <list-box />
     </div>
-    <create-box-modal />
   </div>
 </template>
 
 <script>
 import CreateBoxModal from '../components/CreateBoxModal'
+import ListBoxes from '../components/ListBoxes'
 
 export default {
   components: {
-    CreateBoxModal
+    CreateBoxModal,
+    ListBoxes
   }
 }
 </script>
