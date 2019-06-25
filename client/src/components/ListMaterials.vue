8d3c3a7c03305cfe3607a6dca8caf13610a8a8eb
Jonas Riecken <jonas.riecken00@gmail.com>
2019-06-25 15:09:28 +0200

    now fetching box infos from the database

--- client/src/components/ListMaterials.vue
+++ client/src/components/ListMaterials.vue
@@ -1,17 +1,48 @@
 <template>
-  <tr>
-    <td>Kleber</td>
-    <td>3</td>
-    <td>/</td>
-    <td>OekA</td>
-    <td>Ja</td>
-    <td>Nein</td>
-    <td>bueroshop24</td>
-    <td>
-      <i
-        v-b-modal.EditMaterialModalRef
-        class="far fa-edit float-right pointer"
-      />
-    </td>
-  </tr>
+  <div>
+    <tr
+      v-for="material in materials"
+      :key="material.id"
+    >
+      <td>{{ materials.name }}</td>
+      <td>3</td>
+      <td>/</td>
+      <td>OekA</td>
+      <td>Ja</td>
+      <td>Nein</td>
+      <td>bueroshop24</td>
+      <td>
+        <i
+          v-b-modal.EditMaterialModalRef
+          class="far fa-edit float-right pointer"
+        />
+      </td>
+    </tr>
+  </div>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+
+    }
+  },
+  computed: {
+    materials () {
+      return this.$store.getters['material/list']
+    }
+  },
+  watch: {
+    '$route': {
+      handler: 'fetch',
+      immediate: true
+    }
+  },
+  methods: {
+    async fetch () {
+      await this.$store.dispatch('material/find')
+    }
+  }
+}
+</script>
