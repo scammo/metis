3f06626ec7368c3398a8c1b6b94e531f0c03763f
Daniel Swiatek <daniel.swiatek@codeanker.de>
2019-07-05 15:16:13 +0200

    remove non bootstrap css

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,8 +3,7 @@
     <div>
       <b-navbar
         toggleable="lg"
-        type="dark"
-        class="blue"
+        type="light"
       >
         <router-link
           :to="{ name: 'home'}"
@@ -13,38 +12,35 @@
             LAGERVERWALTUNG
           </b-navbar-brand>
         </router-link>
-        <b-navbar-toggle target="nav-collapse" />
-
-        <b-collapse
-          id="nav-collapse"
-          is-nav
-        >
-          <b-navbar-nav class="mx-auto">
-            <b-nav-form class="search">
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-              />
-            </b-nav-form>
-          </b-navbar-nav>
-          <b-navbar-nav>
-            <b-nav-item-dropdown right>
-              <template slot="button-content">
-                User
-              </template>
-              <b-dropdown-item :to="{ name: 'warehouselocation'}">
-                Settings
-              </b-dropdown-item>
-              <b-dropdown-item href="#">
-                Sign Out
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template slot="button-content">
+              User
+            </template>
+            <b-dropdown-item :to="{ name: 'warehouselocation'}">
+              Settings
+            </b-dropdown-item>
+            <b-dropdown-item href="#">
+              Sign Out
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+       
       </b-navbar>
     </div>
-
+    <pre>{{user}}</pre>
     <router-view />
   </div>
 </template>
+
+<script>
+
+export default {
+  computed: {
+    user () {
+      return this.$store.state.auth.user
+    }
+  }
+}
+
+</script>
